b47b56a621f3300425d509b1fd93b6fe4795a1f5
Kishan <kramolar@gmail.com>
2024-11-16 12:12:37 -0500

    updated code again

--- src/pages/FilledWax.jsx
+++ src/pages/FilledWax.jsx
@@ -6,12 +6,12 @@ import blueWax from "../assets/blueWax.jpg";
 import { ET120FilledWax } from "../components/ET120FilledWax";
 import { ET125FilledWax } from "../components/ET125FilledWax";
 import { ET130FilledWax } from "../components/ET130FilledWax";
+import { BottomBar } from "../components/bottomBar";
 
 export const FilledWax = () => {
   return (
-    <div className="container mx-auto px-8 sm:px-12 md:px-16">
-      <div className="mt-28">
-        
+    <div className="">
+      <div className="mt-28 container mx-auto px-8 sm:px-12 md:px-16">
         {/* Section for ET 120 */}
         <div className="grid grid-cols-1 pt-8 md:grid-cols-2 gap-8 mb-16">
           <div className="flex-1 mx-8 sm:mx-12">
@@ -26,8 +26,14 @@ export const FilledWax = () => {
               <li>Quick setting wax with minimal cavitations / sink age.</li>
               <li>Dimensionally very stable.</li>
               <li>Wide injection range, from paste to liquid.</li>
-              <li>Capable of producing very little release agent for removal from dies.</li>
-              <li>Returns to shape if distortion occurs on removal from the die. Resists cracking.</li>
+              <li>
+                Capable of producing very little release agent for removal from
+                dies.
+              </li>
+              <li>
+                Returns to shape if distortion occurs on removal from the die.
+                Resists cracking.
+              </li>
             </ul>
           </div>
         </div>
@@ -43,12 +49,24 @@ export const FilledWax = () => {
               ET 125 - FILLED PATTERN WAX
             </h2>
             <ul className="mx-8 text-sm sm:text-base lg:text-lg text-cyan-950 space-y-2 list-disc pl-6">
-              <li>A pattern wax specifically for the production of fine precision type castings.</li>
+              <li>
+                A pattern wax specifically for the production of fine precision
+                type castings.
+              </li>
               <li>A medium viscosity, filled, pattern wax.</li>
               <li>Quick setting wax with minimal cavitations/sink age.</li>
-              <li>Can be used as a liquid injection material but performs better if injected at or below the recorded congealing point.</li>
-              <li>Capable of producing patterns of all sizes, and thick or thin sections.</li>
-              <li>Non-sticky, requiring very little release agent for removal from dies.</li>
+              <li>
+                Can be used as a liquid injection material but performs better
+                if injected at or below the recorded congealing point.
+              </li>
+              <li>
+                Capable of producing patterns of all sizes, and thick or thin
+                sections.
+              </li>
+              <li>
+                Non-sticky, requiring very little release agent for removal from
+                dies.
+              </li>
               <li>The wax can be reclaimed from runner systems.</li>
             </ul>
           </div>
@@ -65,16 +83,25 @@ export const FilledWax = () => {
               ET 130 - FILLED PATTERN WAX
             </h2>
             <ul className="mx-8 text-sm sm:text-base lg:text-lg text-cyan-950 space-y-2 list-disc pl-6">
-              <li>A pattern wax specifically for the production of small and big precision type castings.</li>
+              <li>
+                A pattern wax specifically for the production of small and big
+                precision type castings.
+              </li>
               <li>A low viscosity, straight wax compound.</li>
               <li>Good tensile strength.</li>
-              <li>Suitable for injection or gravity pouring and ideal for dewaxing.</li>
+              <li>
+                Suitable for injection or gravity pouring and ideal for
+                dewaxing.
+              </li>
               <li>The wax can be reclaimed for use on runner systems.</li>
               <li>Recommended maximum melting temperature of 100°C.</li>
             </ul>
           </div>
         </div>
         <ET130FilledWax />
+      </div>
+      <div>
+        <BottomBar />
       </div>
     </div>
   );
--- src/pages/UnfilledWax.jsx
+++ src/pages/UnfilledWax.jsx
@@ -1,35 +1,39 @@
 import { SideImageComponent } from "../components/SideImageComponent";
 import comingSoon from "../assets/comingSoon.jpg";
 import { ET250UnFilledWax } from "../components/ET250Un-FilledWax";
+import { BottomBar } from "../components/bottomBar";
 
 export const UnfilledWax = () => {
   return (
-    <div className="container mx-auto px-8 sm:px-12 md:px-16">
-      <div className="grid grid-rows">
-        
-        <div className="flex flex-col pt-12 md:flex-row">
-          <div className="flex-1 mx-8 sm:mx-12">
-            <SideImageComponent imgSrc={comingSoon} />
-          </div>
+    <div>
+      <div className="container mx-auto px-8 sm:px-12 md:px-16">
+        <div className="grid grid-rows">
+          <div className="flex flex-col pt-12 md:flex-row">
+            <div className="flex-1 mx-8 sm:mx-12">
+              <SideImageComponent imgSrc={comingSoon} />
+            </div>
 
-          <div className="flex-1">
-            <h2 className="text-xl sm:text-2xl lg:text-3xl text-cyan-950 mx-8 sm:mx-12 font-semibold mb-4">
-              ET 250 - Un-FILLED PATTERN WAX
-            </h2>
-            <ul className="text-sm sm:text-base lg:text-lg text-cyan-950 space-y-2 list-disc pl-6 mx-8 sm:mx-12">
-              <li>The wax is a low viscosity wax.</li>
-              <li>Quick setting wax with minimal cavitations / sink age.</li>
-              <li>Good surface finish.</li>
-              <li>Can be designed for specialist requirements.</li>
-              <li>Flexible and tough compounds available.</li>
-              <li>High strength wax can be designed.</li>
-            </ul>
+            <div className="flex-1">
+              <h2 className="text-xl sm:text-2xl lg:text-3xl text-cyan-950 mx-8 sm:mx-12 font-semibold mb-4">
+                ET 250 - Un-FILLED PATTERN WAX
+              </h2>
+              <ul className="text-sm sm:text-base lg:text-lg text-cyan-950 space-y-2 list-disc pl-6 mx-8 sm:mx-12">
+                <li>The wax is a low viscosity wax.</li>
+                <li>Quick setting wax with minimal cavitations / sink age.</li>
+                <li>Good surface finish.</li>
+                <li>Can be designed for specialist requirements.</li>
+                <li>Flexible and tough compounds available.</li>
+                <li>High strength wax can be designed.</li>
+              </ul>
+            </div>
           </div>
         </div>
-
+        <div className="mt-8">
+          <ET250UnFilledWax />
+        </div>
       </div>
-      <div className="mt-8">
-        <ET250UnFilledWax />
+      <div>
+        <BottomBar />
       </div>
     </div>
   );
